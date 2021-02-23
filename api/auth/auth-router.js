f5928d81964dc49fb13f7383482cb6c8990d866c
Nathan Hietala <Nrhietala@gmail.com>
2021-02-22 21:10:38 -0600

    working on auth router

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const User = require("../users/users-model");
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-  const { username, password } = req.body;
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashed = bcrypt.hashSync(password, 10);
+
+    const userData = await User.add({ username, password: hashed });
+    res.status(201).json(userData);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 });
 
 router.post("/login", (req, res) => {});
